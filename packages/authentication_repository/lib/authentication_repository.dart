9a622af6a7d3df4d6814a8e08cc9aa728689bbe0
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-10 18:55:53 -0500

    Created the authentication repository

---
+++ packages/authentication_repository/lib/authentication_repository.dart
@@ -0,0 +1,4 @@
+/// Package to manage the authentication domain
+library authentication_repository;
+
+export 'src/authentication_repository.dart';
---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,7 @@
+/// {@template authentication_repository}
+/// Package to manage the authentication domain
+/// {@endtemplate}
+class AuthenticationRepository {
+  /// {@macro authentication_repository}
+  const AuthenticationRepository();
+}

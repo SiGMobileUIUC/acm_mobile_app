79234d9dbe323cf1507dd80a142d4879c331d89c
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-11 17:25:13 -0500

    Added dependencies and implemented backend API interface for the fake implementation

--- packages/backend_api_fake/lib/src/backend_api_fake.dart
+++ packages/backend_api_fake/lib/src/backend_api_fake.dart
@@ -1,7 +1,35 @@
+import 'package:backend_api_interface/backend_api_interface.dart';
+import 'package:dartz/dartz.dart';
+import 'package:failure_models/failure_models.dart';
+
 /// {@template backend_api_fake}
 /// Fake implementation of the backend API interface
 /// {@endtemplate}
-class BackendApiFake {
+class BackendApiFake implements BackendApiInterface {
   /// {@macro backend_api_fake}
   const BackendApiFake();
+
+  @override
+  Future<Either<NetworkFailure, List<EventDto>>> getAllEvents() {
+    // TODO: implement getAllEvents
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<NetworkFailure, List<SIGDto>>> getAllSIGs() {
+    // TODO: implement getAllSIGs
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<NetworkFailure, EventDto>> getEvent(EventId eventId) {
+    // TODO: implement getEvent
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<NetworkFailure, SIGDto>> getSIG(SIGId sigId) {
+    // TODO: implement getSIG
+    throw UnimplementedError();
+  }
 }

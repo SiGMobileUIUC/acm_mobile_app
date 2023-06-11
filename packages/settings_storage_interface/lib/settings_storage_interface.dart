de1dbc79a8e408823bc310679a9bf045f0be8b9f
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-10 23:09:04 -0500

    Created the settings storage interface

---
+++ packages/settings_storage_interface/lib/settings_storage_interface.dart
@@ -0,0 +1,4 @@
+/// Interface and models for storing the settings
+library settings_storage_interface;
+
+export 'src/settings_storage_interface.dart';
---
+++ packages/settings_storage_interface/test/src/settings_storage_interface_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:settings_storage_interface/settings_storage_interface.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('SettingsStorageInterface', () {
+    test('can be instantiated', () {
+      expect(SettingsStorageInterface(), isNotNull);
+    });
+  });
+}

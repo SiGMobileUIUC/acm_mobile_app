551cc021bd47dc3c69752dfb80bcdea606cc586c
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-11 15:52:05 -0500

    Created the failure models package

---
+++ packages/failure_models/lib/failure_models.dart
@@ -0,0 +1,4 @@
+/// Representation of all failures
+library failure_models;
+
+export 'src/failure_models.dart';
---
+++ packages/failure_models/lib/src/failure_models.dart
@@ -0,0 +1,7 @@
+/// {@template failure_models}
+/// Representation of all failures
+/// {@endtemplate}
+class FailureModels {
+  /// {@macro failure_models}
+  const FailureModels();
+}

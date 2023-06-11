ac1981a99df7da8ba01191d94806cd9042196d9c
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-11 12:26:05 -0500

    Added MultiBlocProvider to app

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,9 @@
+import 'package:acm_mobile_app/authentication/bloc/authentication_bloc.dart';
+import 'package:acm_mobile_app/di/di.dart';
 import 'package:acm_mobile_app/l10n/l10n.dart';
 import 'package:acm_mobile_app/routing/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   App({super.key});
@@ -9,16 +12,23 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _appRouter.config(),
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => getIt<AuthenticationBloc>(),
         ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: _appRouter.config(),
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+          colorScheme: ColorScheme.fromSwatch(
+            accentColor: const Color(0xFF13B9FF),
+          ),
+        ),
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
     );
   }
 }

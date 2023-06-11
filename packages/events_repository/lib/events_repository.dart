c9d2c5fa55cdf347f6e47504be81dcc6a25075f9
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-10 23:08:29 -0500

    Created the events repository

---
+++ packages/events_repository/lib/events_repository.dart
@@ -0,0 +1,4 @@
+/// Repositor to manage the events domain
+library events_repository;
+
+export 'src/events_repository.dart';
---
+++ packages/events_repository/lib/src/events_repository.dart
@@ -0,0 +1,7 @@
+/// {@template events_repository}
+/// Repositor to manage the events domain
+/// {@endtemplate}
+class EventsRepository {
+  /// {@macro events_repository}
+  const EventsRepository();
+}

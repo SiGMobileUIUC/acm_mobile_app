1afebcd8f7242b570056ecc865386bde4fe3cd91
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-10 19:48:35 -0500

    Added splash page

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:acm_mobile_app/login/login.dart';
+import 'package:acm_mobile_app/splash/splash.dart';
 import 'package:auto_route/auto_route.dart';
 
 part 'app_router.gr.dart';
@@ -7,6 +8,7 @@ part 'app_router.gr.dart';
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(page: LoginRoute.page, initial: true),
+        AutoRoute(page: SplashRoute.page, initial: true),
+        AutoRoute(page: LoginRoute.page),
       ];
 }
--- lib/routing/app_router.gr.dart
+++ lib/routing/app_router.gr.dart
@@ -20,7 +20,13 @@ abstract class _$AppRouter extends RootStackRouter {
         routeData: routeData,
         child: const LoginPage(),
       );
-    }
+    },
+    SplashRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const SplashPage(),
+      );
+    },
   };
 }
 
@@ -34,6 +40,20 @@ class LoginRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'LoginRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [SplashPage]
+class SplashRoute extends PageRouteInfo<void> {
+  const SplashRoute({List<PageRouteInfo>? children})
+      : super(
+          SplashRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SplashRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }

5157ff05a28ba57e25093949d653aac99f60e6ab
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-11 12:26:44 -0500

    Updated authentication repository DI to dispose correctly

--- lib/di/di.config.dart
+++ lib/di/di.config.dart
@@ -10,19 +10,19 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:acm_mobile_app/authentication/bloc/authentication_bloc.dart'
-    as _i11;
-import 'package:acm_mobile_app/di/modules.dart' as _i12;
-import 'package:acm_mobile_app/events/bloc/events_bloc.dart' as _i4;
-import 'package:acm_mobile_app/home/bloc/home_bloc.dart' as _i5;
-import 'package:acm_mobile_app/login/bloc/login_bloc.dart' as _i6;
-import 'package:acm_mobile_app/profile/bloc/profile_bloc.dart' as _i7;
-import 'package:acm_mobile_app/settings/bloc/settings_bloc.dart' as _i8;
-import 'package:acm_mobile_app/sigs/bloc/sigs_bloc.dart' as _i9;
+    as _i12;
+import 'package:acm_mobile_app/di/modules.dart' as _i4;
+import 'package:acm_mobile_app/events/bloc/events_bloc.dart' as _i5;
+import 'package:acm_mobile_app/home/bloc/home_bloc.dart' as _i6;
+import 'package:acm_mobile_app/login/bloc/login_bloc.dart' as _i7;
+import 'package:acm_mobile_app/profile/bloc/profile_bloc.dart' as _i8;
+import 'package:acm_mobile_app/settings/bloc/settings_bloc.dart' as _i9;
+import 'package:acm_mobile_app/sigs/bloc/sigs_bloc.dart' as _i10;
 import 'package:authentication_repository/authentication_repository.dart'
     as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:user_repository/user_repository.dart' as _i10;
+import 'package:user_repository/user_repository.dart' as _i11;
 
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -37,18 +37,20 @@ extension GetItInjectableX on _i1.GetIt {
     );
     final registerModule = _$RegisterModule();
     gh.singleton<_i3.AuthenticationRepository>(
-        registerModule.authenticationRepository);
-    gh.lazySingleton<_i4.EventsBloc>(() => _i4.EventsBloc());
-    gh.lazySingleton<_i5.HomeBloc>(() => _i5.HomeBloc());
-    gh.lazySingleton<_i6.LoginBloc>(() => _i6.LoginBloc());
-    gh.lazySingleton<_i7.ProfileBloc>(() => _i7.ProfileBloc());
-    gh.lazySingleton<_i8.SettingsBloc>(() => _i8.SettingsBloc());
-    gh.lazySingleton<_i9.SigsBloc>(() => _i9.SigsBloc());
-    gh.singleton<_i10.UserRepository>(registerModule.userRepository);
-    gh.singleton<_i11.AuthenticationBloc>(
-      _i11.AuthenticationBloc(
+      registerModule.authenticationRepository,
+      dispose: _i4.disposeAuthenticationRepository,
+    );
+    gh.lazySingleton<_i5.EventsBloc>(() => _i5.EventsBloc());
+    gh.lazySingleton<_i6.HomeBloc>(() => _i6.HomeBloc());
+    gh.lazySingleton<_i7.LoginBloc>(() => _i7.LoginBloc());
+    gh.lazySingleton<_i8.ProfileBloc>(() => _i8.ProfileBloc());
+    gh.lazySingleton<_i9.SettingsBloc>(() => _i9.SettingsBloc());
+    gh.lazySingleton<_i10.SigsBloc>(() => _i10.SigsBloc());
+    gh.singleton<_i11.UserRepository>(registerModule.userRepository);
+    gh.singleton<_i12.AuthenticationBloc>(
+      _i12.AuthenticationBloc(
         authenticationRepository: gh<_i3.AuthenticationRepository>(),
-        userRepository: gh<_i10.UserRepository>(),
+        userRepository: gh<_i11.UserRepository>(),
       ),
       dispose: (i) => i.close(),
     );
@@ -56,10 +58,10 @@ extension GetItInjectableX on _i1.GetIt {
   }
 }
 
-class _$RegisterModule extends _i12.RegisterModule {
+class _$RegisterModule extends _i4.RegisterModule {
   @override
   _i3.AuthenticationRepository get authenticationRepository =>
       _i3.AuthenticationRepository();
   @override
-  _i10.UserRepository get userRepository => _i10.UserRepository();
+  _i11.UserRepository get userRepository => _i11.UserRepository();
 }
--- lib/di/modules.dart
+++ lib/di/modules.dart
@@ -1,12 +1,20 @@
+import 'dart:async';
+
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:injectable/injectable.dart';
 import 'package:user_repository/user_repository.dart';
 
 @module
 abstract class RegisterModule {
-  @singleton
+  @Singleton(dispose: disposeAuthenticationRepository)
   AuthenticationRepository get authenticationRepository;
 
   @singleton
   UserRepository get userRepository;
+}
+
+FutureOr<void> disposeAuthenticationRepository(
+  AuthenticationRepository instance,
+) {
+  instance.dispose();
 }

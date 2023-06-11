10f986907ccb32ace3c2b8f835b2c992ff851e23
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-10 20:19:26 -0500

    Created the backend API implementation using Dio

---
+++ packages/backend_api_dio/lib/src/backend_api_dio.dart
@@ -0,0 +1,7 @@
+/// {@template backend_api_dio}
+/// Implemention of the backend API interface using Dio for HTTP requests
+/// {@endtemplate}
+class BackendApiDio {
+  /// {@macro backend_api_dio}
+  const BackendApiDio();
+}

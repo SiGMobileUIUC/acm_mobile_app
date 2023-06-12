e583103450dc48f11f2ee5fc5705caf265231e94
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-12 00:25:45 -0500

    Created local storage interface

---
+++ packages/local_storage_interface/lib/src/local_storage_interface.dart
@@ -0,0 +1,7 @@
+/// {@template local_storage_interface}
+/// Interface for local storage
+/// {@endtemplate}
+class LocalStorageInterface {
+  /// {@macro local_storage_interface}
+  const LocalStorageInterface();
+}

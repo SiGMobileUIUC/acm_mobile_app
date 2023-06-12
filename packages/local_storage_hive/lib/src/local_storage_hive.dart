e44bce6c5b8304d910699dfa9ffae1c9cb59f3ee
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-12 00:28:31 -0500

    Created the hive implementation of local storage interface

---
+++ packages/local_storage_hive/lib/src/local_storage_hive.dart
@@ -0,0 +1,7 @@
+/// {@template local_storage_hive}
+/// Hive implementation of the local storage interface
+/// {@endtemplate}
+class LocalStorageHive {
+  /// {@macro local_storage_hive}
+  const LocalStorageHive();
+}

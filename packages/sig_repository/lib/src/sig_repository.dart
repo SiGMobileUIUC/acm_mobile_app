5e7ffa61be4216efb332cf32911fe1241ad59b1e
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-12 01:20:51 -0500

    Added local storage logic to SIG repository

--- packages/sig_repository/lib/src/sig_repository.dart
+++ packages/sig_repository/lib/src/sig_repository.dart
@@ -1,6 +1,7 @@
 import 'package:backend_api_interface/backend_api_interface.dart';
 import 'package:dartz/dartz.dart';
 import 'package:failure_models/failure_models.dart';
+import 'package:local_storage_interface/local_storage_interface.dart';
 
 import 'package:sig_repository/src/models/models.dart';
 
@@ -11,9 +12,12 @@ class SIGRepository {
   /// {@macro sig_repository}
   const SIGRepository({
     required BackendApiInterface backendApiInterface,
-  }) : _backendApiInterface = backendApiInterface;
+    required LocalStorageInterface localStorageInterface,
+  })  : _backendApiInterface = backendApiInterface,
+        _localStorageInterface = localStorageInterface;
 
   final BackendApiInterface _backendApiInterface;
+  final LocalStorageInterface _localStorageInterface;
 
   Future<Either<NetworkFailure, List<SIG>>> getAllSIGs() async {
     final failureOrSIGDtos = await _backendApiInterface.getAllSIGs();
@@ -21,5 +25,21 @@ class SIGRepository {
       left,
       (sigDtos) => right(sigDtos.map(SIG.fromDto).toList()),
     );
+  }
+
+  void toggleFavorite({required SIG sig}) {
+    if (sig.favorite) {
+      _localStorageInterface.favoriteSIG(sig.id);
+    } else {
+      _localStorageInterface.unfavoriteSIG(sig.id);
+    }
+  }
+
+  void toggleNotifications({required SIG sig}) {
+    if (sig.favorite) {
+      _localStorageInterface.favoriteSIG(sig.id);
+    } else {
+      _localStorageInterface.unfavoriteSIG(sig.id);
+    }
   }
 }

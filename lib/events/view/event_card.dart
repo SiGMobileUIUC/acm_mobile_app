e86849e7986d966d6657ae55827b42505df3cf95
Apoorva Aditya <apoorvaufo3@gmail.com>
2023-06-14 00:03:09 -0500

    Started work on events page

---
+++ lib/events/view/event_card.dart
@@ -0,0 +1,16 @@
+import 'package:events_repository/events_repository.dart';
+import 'package:flutter/material.dart';
+
+class EventCard extends StatelessWidget {
+  const EventCard({
+    required this.event,
+    super.key,
+  });
+
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/events/view/events.dart
+++ lib/events/view/events.dart
@@ -1,12 +1,29 @@
+import 'package:acm_mobile_app/common/widgets/loading_progress_indicator.dart';
+import 'package:acm_mobile_app/events/bloc/events_bloc.dart';
+import 'package:acm_mobile_app/events/view/event_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EventsTab extends StatelessWidget {
   const EventsTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return BlocBuilder<EventsBloc, EventsState>(
+      builder: (context, state) {
+        return state.map(
+          initial: (_) => const LoadingProgressIndicator(),
+          loading: (_) => const LoadingProgressIndicator(),
+          loaded: (state) {
+            final events = state.events;
+            return ListView.builder(
+              itemBuilder: (_, index) => EventCard(event: events[index]),
+              itemCount: events.length,
+            );
+          },
+          error: (failure) => Container(),
+        );
+      },
     );
   }
 }
